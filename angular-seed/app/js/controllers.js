a6bb422fb231cec9c1e7812dbe05d3d701ad019e
mymachine8 <mymachine8@gmail.com>
2014-05-22 02:03:35 +0530

    ISBN Template creation

--- angular-seed/app/js/controllers.js
+++ angular-seed/app/js/controllers.js
@@ -5,6 +5,8 @@
 angular.module('myApp.controllers', [])
   .controller('MyCtrl1', ['$scope', function($scope) {
 
+  	$scope.displayText = "Default Shit";
+  	$scope.addBook = function (){ $scope.displayText = "This shit is working"};
   }])
   .controller('MyCtrl2', ['$scope', function($scope) {
 
